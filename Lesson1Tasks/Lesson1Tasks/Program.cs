7fcc1fb9f9cbdf1307e08608edd1ba1cb9485251
unknown <rostykvoznyak@gmail.com>
2020-10-22 20:16:57 +0300

    Done Lesson1 Task1

---
+++ Lesson1Tasks/Lesson1Tasks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson1Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 0;
+            sbyte b = -8;
+            ushort c = 67;
+            short d = -768;
+            uint e = 11;
+            int f = -483648;
+            ulong k = 89;
+            long o = -908;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(k);
+            Console.WriteLine(o);
+
+
+
+
+        }
+    }
+}
